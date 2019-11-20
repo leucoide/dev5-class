3fd024258793d45fb307597d4488f17e9b1e61a1
yvasquez <yuri.vasquez@tvglobo.com.br>
2019-11-20 14:53:21 -0300

    dynamic style

---
+++ 3_dynamic_style/index.js
@@ -0,0 +1,7 @@
+function loadItemHandler () {
+    // selecionar todos os elementos "li"
+    // escutar o evento de click em cada um deles
+    // remover ou adicionar a classe done conforme necessário
+}
+
+loadItemHandler()
